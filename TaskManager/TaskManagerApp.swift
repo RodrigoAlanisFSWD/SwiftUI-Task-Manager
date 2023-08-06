fb68da18d5f84fc6576ce9f1ab59d35000dc098d
RodrigoAlanisFSWD <rodrigoalanisfswd@gmail.com>
2023-08-06 15:18:27 -0600

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Rodrigo Alanis Lemus on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
